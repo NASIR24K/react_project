78e969a770a7023a25fa75fcea040534ed497a20
nfsoft <nasir24k@gmail.com>
2023-08-22 15:25:25 +0600

    ReactJs, React-bootstrap using portfolio website

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
@@ -13,11 +13,12 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
         },
       },
     ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 3000,
   },
 };
